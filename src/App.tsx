f64df974a0fb8f83c051ccd1293f2e9c75c6d107
rui-watanabe <wataru610806@gmail.com>
2020-06-27 11:57:39 +0900

    defined props function type

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
+
+import { Todo } from './todo.model'
 
 // function App() {
 //   return (
@@ -10,9 +13,9 @@ import NewTodo from './components/NewTodo';
 // }
 
 const App: React.FC = () => {
-  const todos = [{id: '1', text: 'typescript'}]
+  const [todos, setTodos] = useState<Todo[]>([]);
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    setTodos(prevTodos => [...prevTodos, {id: Math.random().toString(), text}])
   };
 
   return (
