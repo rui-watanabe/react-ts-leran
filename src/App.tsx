2c4186f9036abf5843600e9f40df444c22672ae6
rui-watanabe <wataru610806@gmail.com>
2020-06-27 11:44:49 +0900

    create newtodo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
 // function App() {
 //   return (
@@ -9,9 +10,14 @@ import TodoList from './components/TodoList';
 // }
 
 const App: React.FC = () => {
-  const  todos = [{id: '1', text: 'typescript'}]
+  const todos = [{id: '1', text: 'typescript'}]
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
+
   return (
     <div className="App">
+      <NewTodo onAddTodo={ todoAddHandler } />
       <TodoList todos={ todos } />
     </div>
   );
